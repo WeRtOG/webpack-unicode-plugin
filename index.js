b532e771591369292a22f6173ffc42a0305e0978
AlinaTkachenko <alina.tkachenko.ca@gmail.com>
2024-04-05 11:18:03 -0700

    Deprecation and bug fixes

--- index.js
+++ index.js
@@ -1,21 +1,36 @@
 const { escapeNonUnicodeCharacters, shouldProcessFile } = require("./utils");
+const { Compilation } = require("webpack");
 
 class UnicodePlugin {
   apply(compiler) {
-    compiler.hooks.emit.tapAsync("UnicodePlugin", (compilation, callback) => {
-      Object.keys(compilation.assets).forEach((filename) => {
-        let source = compilation.assets[filename].source();
+    // Utilizing the 'processAssets' hook with an appropriate stage
+    compiler.hooks.compilation.tap("UnicodePlugin", (compilation) => {
+      compilation.hooks.processAssets.tapAsync(
+        {
+          name: "UnicodePlugin",
+          // Specify the stage to ensure your plugin runs at the correct time
+          // Adjust the stage based on when you want your plugin to run
+          stage: Compilation.PROCESS_ASSETS_STAGE_OPTIMIZE,
+        },
+        (assets, callback) => {
+          // Iterate over all the assets
+          Object.keys(assets).forEach((filename) => {
+            if (shouldProcessFile(filename)) {
+              let source = assets[filename].source();
+              if (typeof source === "string") {
+                source = escapeNonUnicodeCharacters(source);
 
-        if (shouldProcessFile(filename)) {
-          source = escapeNonUnicodeCharacters(source);
-
-          compilation.assets[filename] = {
-            source: () => source,
-            size: () => source.length,
-          };
+                // Update the asset
+                assets[filename] = {
+                  source: () => source,
+                  size: () => source.length,
+                };
+              }
+            }
+          });
+          callback();
         }
-      });
-      callback();
+      );
     });
   }
 }
